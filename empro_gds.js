759678c04702c4b9f3d057f7c8ba7e5e0c0fffd7
verint-empro-marketplace <79576338+verint-empro-marketplace@users.noreply.github.com>
2021-03-22 16:16:24 +0700

    Update empro_gds.js
    add new function

--- empro_gds.js
+++ empro_gds.js
@@ -1,3 +1,57 @@
+/**
+=====================================
+        ACCORDION - START
+=====================================
+**/
+function addAccordion(){
+	console.log('accordion has started');
+	var elements = $('.accordion');
+	for (var i = 0; i < elements.length; i++) {
+	    var element = elements[i];
+	    var search_widget_name = $(element).find('fieldset[data-type="search"]').data('field');
+	    var label;
+
+	    if (search_widget_name.toLowerCase().indexOf("customer") >= 0) {
+		label = "individual";
+	    }
+	    else if (search_widget_name.toLowerCase().indexOf("organization") >= 0) {
+		label = "organization";
+	    }
+	    else if (search_widget_name.toLowerCase().indexOf("property") >= 0) {
+		label = "property";
+	    }
+	    else {
+		label = "street";
+	    }
+
+	    $(element).find('fieldset[data-type="search"]').wrap('<div class="accordion_container" data-for="' + search_widget_name + '"></div>');
+
+	    $('.accordion_container[data-for="' + search_widget_name + '"]').prepend('<label class="accordion_label" data-for="' + search_widget_name + '">Search for ' + label + '</label>');
+
+	    $(element).find('fieldset[data-type="search"]').wrap('<div class="accordion_content" data-for="' + search_widget_name + '" style></div>');
+
+	    $('.accordion_label[data-for="' + search_widget_name + '"]').click(function () {
+		$(this).toggleClass("closed");
+		var data = $(this).data('for');
+		var isClosed = $(this).hasClass('closed');
+		if (isClosed) {
+		    $('.accordion_content[data-for="' + data + '"]').hide();
+		} else {
+		    $('.accordion_content[data-for="' + data + '"]').show();
+		}
+	    });
+	}
+	if (KDF.getVal('txt_customer_id') !== '' && typeof KDF.getParams().customerid !== 'undefined') {
+		$('.accordion_label[data-for="dform_widget_cs_customer_search_id"]').click();
+// 		$('.accordion_label[data-for="dform_widget_ps_citizen_property_search_id"]').click();
+	}
+}//end addAccordion
+/**
+=====================================
+        ACCORDION - END
+=====================================
+**/
+
 function toggleDebugStyle(){debugStyle = !debugStyle;} var debugStyle = false;
 /*  //KS: put in _KDF_ready - uses all the reccomended styles - can add optional
 applyStyle(['recommended']);
