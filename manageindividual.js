8efc1328ea5680a6d4c2d4bb0282da9d55c0969e
verint-empro-marketplace <79576338+verint-empro-marketplace@users.noreply.github.com>
2021-05-18 15:42:01 +0700

    update reporter
    update reporter

--- manageindividual.js
+++ manageindividual.js
@@ -399,7 +399,7 @@ function do_KDF_Custom_Individual(event, kdf, response, action) {
 				KDF.showSuccess('Individual Details Created');
 				KDF.showInfo('Individual Details Created');
 				KDF.setVal('txt_customer_id', response.data.txt_customerID);
-				//KDF.setCustomerID(response.data.txt_customerID, true, false); /*set Reporter*/
+				KDF.setCustomerID(response.data.txt_customerID, true, false); /*set Reporter*/
 				KDF.customdata('person-retrieve-new', individualTemplateIdentifier + 'create-individual', true, true, { 'person_search_results': KDF.getVal('txt_customer_id') });
 			}
 		}
