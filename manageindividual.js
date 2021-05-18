9c5f7c668cb8fbd30137812b8cd88204bebde0c2
verint-empro-marketplace <79576338+verint-empro-marketplace@users.noreply.github.com>
2021-05-18 17:25:58 +0700

    associate newly created customer
    associate newly created customer

--- manageindividual.js
+++ manageindividual.js
@@ -400,6 +400,7 @@ function do_KDF_Custom_Individual(event, kdf, response, action) {
 				KDF.showInfo('Individual Details Created');
 				KDF.setVal('txt_customer_id', response.data.txt_customerID);
 				KDF.setCustomerID(response.data.txt_customerID, true, false); /*set Reporter*/
+				KDF.setVal('le_associated_obj_id',response.data.txt_customerID);
 				KDF.customdata('person-retrieve-new', individualTemplateIdentifier + 'create-individual', true, true, { 'person_search_results': KDF.getVal('txt_customer_id') });
 			}
 		}
