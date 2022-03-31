2c5fc7722982f181a8378f3b59c1de24e703941a
verint-empro-marketplace <79576338+verint-empro-marketplace@users.noreply.github.com>
2022-03-31 17:33:29 +0700

    hide manual address when property selected

--- simple_manageindividual.js
+++ simple_manageindividual.js
@@ -173,7 +173,7 @@ function do_KDF_objectdataLoaded_Individual(event, kdf, response, type, id) {
 	    KDF.setVal('txt_city_yd', response["profile-City"]);
 	    KDF.setVal('txt_postcode_yd', response["profile-Postcode"]);
 	    
-	    hideWidgets(['txt_address_number_yd','txt_city_yd','txt_street_name_yd']);
+	    hideWidgets(['txt_address_number_yd','txt_city_yd','txt_street_name_yd','but_yd_edit_address']);
 	    KDF.setVal('txta_address_yd',KDF.getVal('txt_address_number_yd')+', '+KDF.getVal('txt_street_name_yd')+', '+KDF.getVal('txt_city_yd')+', '+KDF.getVal('txt_postcode_yd'));
 		
 	    //set default value for placeholder fields used in update individual
